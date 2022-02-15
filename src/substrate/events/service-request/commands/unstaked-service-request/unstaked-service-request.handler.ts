a273ac12cd10f682c22654e51f559287cd65c7e5
rubenkristian <rubenkristian96@gmail.com>
2022-02-15 12:55:09 +0700

    update contries index delete services

--- src/substrate/events/service-request/commands/unstaked-service-request/unstaked-service-request.handler.ts
+++ src/substrate/events/service-request/commands/unstaked-service-request/unstaked-service-request.handler.ts
@@ -27,5 +27,39 @@ export class UnstakedServiceRequestHandler
         },
       },
     });
+
+    const { body } = await this.elasticsearchService.search({
+      index: 'create-service-request',
+      body: {
+        query: {
+          match: { _id: command.request.hash }
+        }
+      }
+    });
+
+    const requestService = body.hits.hits[0];
+    if (requestService) {
+      const source = requestService._source;
+      const service = `${source.request.hash}-${source.request.country}-${source.request.region}-${source.request.city}-${source.request.service_category}-${source.request.staking_amount}`;
+
+      await this.elasticsearchService.update({
+        index: 'country-service-request',
+        id: source.request.country,
+        refresh: 'wait_for',
+        body: {
+          script: {
+            lang: 'painless',
+            source: `
+              if (ctx._source.services.contains(params.service)) {
+                ctx._source.services.remove(ctx._source.services.indexOf(params.service));
+              }
+            `,
+            params: {
+              service: service
+            }
+          }
+        }
+      });
+    }
   }
 }
