fe6ad3e0eca484274ac1f629cc96b951896a674e
rubenkristian <rubenkristian96@gmail.com>
2022-02-15 12:54:27 +0700

    contries indexer

--- src/substrate/events/service-request/commands/claimed-service-request/claimed-service-request.handler.ts
+++ src/substrate/events/service-request/commands/claimed-service-request/claimed-service-request.handler.ts
@@ -32,5 +32,39 @@ export class ClaimedServiceRequestHandler
         },
       },
     });
+
+    const { body } = await this.elasticSearchService.search({
+      index: 'create-service-request',
+      body: {
+        query: {
+          match: { _id: command.claimRequest.requestHash }
+        }
+      }
+    });
+
+    const requestService = body.hits.hits[0];
+    if (requestService) {
+      const source = requestService._source;
+      const service = `${source.request.hash}-${source.request.country}-${source.request.region}-${source.request.city}-${source.request.service_category}-${source.request.staking_amount}`;
+
+      await this.elasticSearchService.update({
+        index: 'country-service-request',
+        id: source.request.country,
+        refresh: 'wait_for',
+        body: {
+          script: {
+            lang: 'painless',
+            source: `
+              if (ctx._source.services.contains(params.service)) {
+                ctx._source.services.remove(ctx._source.services.indexOf(params.service));
+              }
+            `,
+            params: {
+              service: service
+            }
+          }
+        }
+      });
+    }
   }
 }
--- src/substrate/events/service-request/commands/create-service-request/create-service-request.handler.ts
+++ src/substrate/events/service-request/commands/create-service-request/create-service-request.handler.ts
@@ -20,5 +20,49 @@ export class CreateServiceRequestHandler
         blockMetadata: command.blockMetadata,
       },
     });
+
+    const { body } = await this.elasticsearchService.search({
+      index: 'country-service-request',
+      body: {
+        query: {
+          match: { _id: command.request.country }
+        }
+      },
+      allow_no_indices: true
+    });
+
+    const service = `${command.request.hash}-${command.request.country}-${command.request.region}-${command.request.city}-${command.request.service_category}-${command.request.staking_amount}`;
+
+    if (body.hits.hits[0]) {
+      await this.elasticsearchService.update({
+        index: 'country-service-request',
+        id: command.request.country,
+        refresh: 'wait_for',
+        body: {
+          script: {
+            lang: 'painless',
+            source: `
+              if (!ctx._source.services.contains(params.service)) {
+                ctx._source.services.add(params.service);
+              }
+            `,
+            params: {
+              service: service
+            }
+          }
+        }
+      });
+    } else {
+      const services = [];
+      services.push(service);
+      await this.elasticsearchService.index({
+        index: 'country-service-request',
+        id: command.request.country,
+        refresh: 'wait_for',
+        body: {
+          services: services
+        }
+      });
+    }
   }
 }
